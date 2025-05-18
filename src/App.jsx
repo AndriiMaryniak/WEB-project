5623fa7fe451404c2665c8f1bed7dfa605949f0e
Андрій bc2024-3 <andrewmarynyak@gmail.com>
2025-05-18 04:51:47 +0300

    Реалізація отримання даних через API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import MovieList from './components/MovieList';
-import { movies } from './data/movies';
+import Loader from './components/Loader';
 import './index.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/movies');
+        setMovies(response.data);
+      } catch (error) {
+        console.error('Помилка завантаження фільмів:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <header className="header">
-        <h1 className="mainTitle">CineMagic</h1>
+        <h1 className="mainTitle">Cinema no Takizawa</h1>
         <p className="subtitle">Оберіть фільм та час сеансу</p>
       </header>
-      <MovieList movies={movies} />
+      
+      {loading ? <Loader /> : <MovieList movies={movies} />}
     </div>
   );
 }
